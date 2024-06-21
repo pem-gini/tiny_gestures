31459224e2ec208c612641d790a22afb324f4113
PraxisUnterwegs <zhuofan.jiang2233@gmail.com>
2024-06-21 11:11:16 +0200

    could generate by it self

--- step2__start_training.sh
+++ step2__start_training.sh
@@ -11,23 +11,20 @@ if [ $? -ne 0 ]; then
 fi
 
 # Write a file to put classes' labels into it
-echo -e "ok\nstop\npalm\nfist\nlike\npeace\npeace_inv" > classes.txt
+echo -e "ok\nstop\npalm\nfist\nlike\npeace\npeace_inv" > "$TINY_GESTURES_DIR/classes.txt"
 
-classes_file="TINY_GESTURES_DIR/classes.txt"
+classes_file="$TINY_GESTURES_DIR/classes.txt"
 
 if [ -f "$classes_file" ]; then
     num_categories=$(grep -v '^[[:space:]]*$' "$classes_file" | wc -l)
-
-content=$(cat << 'EOF'
-classes = 7
-train = $TINY_GESTURES_DIR/images_train.txt
-valid = $TINY_GESTURES_DIR/images_val.txt
-names = $TINY_GESTURES_DIR/obj.name
-backup = $TINY_GESTURES_DIR/backup/
-EOF
-)
-
-echo "$content" > training_control_file.txt
+    {
+    echo -e "classes = $num_categories"
+    echo -e "train = $TINY_GESTURES_DIR/images_train.txt"
+    echo -e "valid = $TINY_GESTURES_DIR/images_val.txt"
+    echo -e "names = $TINY_GESTURES_DIR/classes.txt"
+    echo -e "backup = $TINY_GESTURES_DIR/backup/"
+    } > "$TINY_GESTURES_DIR/training_control_file.txt"
+fi
 
 # Get the necessary file path
 OBJ_DATA_PATH="$TINY_GESTURES_DIR/training_control_file.txt"
