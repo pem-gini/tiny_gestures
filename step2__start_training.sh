e308157d2373d07a7b01bfa0386b278f764801dd
PraxisUnterwegs <zhuofan.jiang2233@gmail.com>
2024-06-21 10:33:47 +0200

    generate file

--- step2__start_training.sh
+++ step2__start_training.sh
@@ -10,17 +10,27 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Find the path of obj.data
-OBJ_DATA_FILE="$TINY_GESTURES_DIR/obj.data"
+# Write a file to put classes' labels into it
+echo -e "ok\nstop\npalm\nfist\nlike\npeace\npeace_inv" > classes.txt
 
-# Use the sed command to modify the path in the obj.data file
-sed -i "s|^train.*|train = $TINY_GESTURES_DIR/images_train.txt|" "$OBJ_DATA_FILE"
-sed -i "s|^valid.*|valid = $TINY_GESTURES_DIR/images_val.txt|" "$OBJ_DATA_FILE"
-sed -i "s|^names.*|names = $TINY_GESTURES_DIR/obj.name|" "$OBJ_DATA_FILE"
-sed -i "s|^backup.*|backup = $TINY_GESTURES_DIR/backup/|" "$OBJ_DATA_FILE"
+classes_file="TINY_GESTURES_DIR/classes.txt"
+
+if [ -f "$classes_file" ]; then
+    num_categories=$(grep -v '^[[:space:]]*$' "$classes_file" | wc -l)
+
+content=$(cat << 'EOF'
+classes = 7
+train = $TINY_GESTURES_DIR/images_train.txt
+valid = $TINY_GESTURES_DIR/images_val.txt
+names = $TINY_GESTURES_DIR/obj.name
+backup = $TINY_GESTURES_DIR/backup/
+EOF
+)
+
+echo "$content" > training_control_file.txt
 
 # Get the necessary file path
-OBJ_DATA_PATH="$TINY_GESTURES_DIR/obj.data"
+OBJ_DATA_PATH="$TINY_GESTURES_DIR/training_control_file.txt"
 CFG_PATH="$TINY_GESTURES_DIR/yolov4-tiny-custom.cfg"
 WEIGHTS_PATH="$TINY_GESTURES_DIR/yolov4-tiny.conv.29"
 
